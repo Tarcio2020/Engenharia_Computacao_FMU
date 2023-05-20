ee28d0840065c8af69ca8233122556a62220e90d
Tarcio2020 <94722456+Tarcio2020@users.noreply.github.com>
2023-05-19 21:28:50 -0300

    Add files via upload

---
+++ operadores_C.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+
+int main(){
+   
+   //Tipos de Dasos
+   // Inteiros
+   // Floats ex.: 1.0, 3.4, -0.6...
+   
+   //Como imprimir esses números na tela?
+   
+   
+   
+    printf("O número %d é inteiro.\n", 2);
+   //console: O número 2 é inteiro
+   //marcador de posição
+   
+     printf("O número %f é inteiro.\n", 5.54545);
+    //console: O número 5.5454 é inteiro
+    //marcador de posição
+   
+   /* Operadores Aritméticos Básicos
+   
+   Soma +
+   Sub -
+   Mult *
+   Divisão /
+   Poteciação **
+   
+   */
+   
+   print("soma de 5 + 2: ")
+   
+    return 0;
+    
+}
+
+
